8408d7fe76976047cd7967dd0fba37c641d4db9b
Takahito Tejima <takahito@pixar.com>
2014-06-09 11:37:04 -0700

    remove transpose from CropV

--- examples/raytrace/osdutil/bezier.h
+++ examples/raytrace/osdutil/bezier.h
@@ -64,26 +64,28 @@ struct BezierCurveEvaluate<T, REAL, N, 0>
    }
 };
 
-template <int N, int I, typename VALUE_TYPE, typename REAL>
+template <int N, int I, int STRIDE, typename VALUE_TYPE, typename REAL>
 struct BezierSplit {
     typedef VALUE_TYPE ValueType;
     typedef REAL Real;
     BezierSplit(ValueType a[], ValueType b[], const ValueType p[], Real t) {
         int tn = N-1-I;//3
-        a[I] = p[0];
-        b[tn] = p[tn];
-        ValueType tmp[N-1];
+        a[I*STRIDE] = p[0];
+        b[tn*STRIDE] = p[tn*STRIDE];
+        ValueType tmp[(N-1)*STRIDE];
         for (int j = 0; j < tn; j++) {
-            tmp[j] = (1-t)*p[j] + t*p[j+1];//p[j] + t*(p[j+1] - p[j])
+            tmp[j*STRIDE] = (1-t)*p[j*STRIDE] + t*p[(j+1)*STRIDE];//p[j] + t*(p[j+1] - p[j])
         }
-        BezierSplit<N, I+1, VALUE_TYPE, REAL>(a, b, &tmp[0], t);
+        BezierSplit<N, I+1, STRIDE, VALUE_TYPE, REAL>(a, b, &tmp[0], t);
     }
 };
-template <int N, typename VALUE_TYPE, typename REAL>
-struct BezierSplit<N, N, VALUE_TYPE, REAL> {
+
+template <int N, int STRIDE, typename VALUE_TYPE, typename REAL>
+struct BezierSplit<N, N, STRIDE, VALUE_TYPE, REAL> {
     typedef VALUE_TYPE ValueType;
     typedef REAL Real;
-    BezierSplit(ValueType[], ValueType[], const ValueType[], Real) {}
+    BezierSplit(ValueType[], ValueType[], const ValueType[], Real) {
+    }
 };
 
 template<class VALUE_TYPE, class REAL, int N=4>
@@ -253,14 +255,11 @@ public:
 
     /// crop
     void CropV(This &patch, Real v0, Real v1) const {
-        This tmp(*this);
-        transpose(tmp._cp);
         for (int i = 0; i < N; ++i) {
-            ValueType tmp0[N], tmp1[N];
-            bezierSplit(&tmp0[0], &tmp1[0], &tmp._cp[i*N+0], v1);
-            bezierSplit(&tmp1[0], &patch._cp[i*N+0], &tmp0[0],  v0/v1);
+            ValueType tmp0[N*N], tmp1[N*N];
+            bezierSplitV(&tmp0[i], &tmp1[i], &_cp[i], v1);
+            bezierSplitV(&tmp1[i], &patch._cp[i], &tmp0[i],  v0/v1);
         }
-        transpose(patch._cp);
     }
 
     ValueType GetLv() const {
@@ -282,7 +281,10 @@ public:
 
 private:
     void bezierSplit(ValueType a[], ValueType b[], const ValueType p[], Real t) const {
-        BezierSplit<N, 0, ValueType, Real> split(a, b, p, t);
+        BezierSplit<N, 0, /*stride*/1, ValueType, Real> split(a, b, p, t);
+    }
+    void bezierSplitV(ValueType a[], ValueType b[], const ValueType p[], Real t) const {
+        BezierSplit<N, 0, /*stride*/N, ValueType, Real> split(a, b, p, t);
     }
 
     static void transpose(ValueType m[]) {
